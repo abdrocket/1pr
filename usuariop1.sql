da521c748d10a9da9853618575e2064a736df42b
mizadri <adrigarvk8@gmail.com>
2015-02-25 13:24:17 +0100

    Corrected sql scheme and added usuariop1 creation script

---
+++ usuariop1.sql
@@ -0,0 +1,13 @@
+GRANT USAGE ON *.* TO 'UsuarioP1'@'localhost';
+
+GRANT SELECT ON `Practica1\_606`.* TO 'UsuarioP1'@'localhost';
+
+GRANT UPDATE ON `Practica1_606`.`Usuarios` TO 'UsuarioP1'@'localhost';
+
+GRANT UPDATE (usuario_target, usuario_source) ON `Practica1_606`.`Crucigramas` TO 'UsuarioP1'@'localhost';
+
+GRANT INSERT, UPDATE ON `Practica1_606`.`Historial` TO 'UsuarioP1'@'localhost';
+
+GRANT INSERT, DELETE ON `Practica1_606`.`Amigos` TO 'UsuarioP1'@'localhost';
+
+GRANT INSERT ON `Practica1_606`.`Activos` TO 'UsuarioP1'@'localhost';
